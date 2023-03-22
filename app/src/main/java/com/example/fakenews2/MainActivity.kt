9cd042e7853d058412e607a59a3ec4f83318b8fb
otanerifpb <renato.silva.pro@gmail.com>
2023-03-22 12:27:54 -0300

    Atualização Criar tela Jogo Criar tela Cadastro Transição tela Main para Jogo e Cadastro

--- app/src/main/java/com/example/fakenews2/MainActivity.kt
+++ app/src/main/java/com/example/fakenews2/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.fakenews2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btJogar: Button
+    private lateinit var btCadstrar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.btJogar = findViewById(R.id.btJogar)
+        this.btCadstrar = findViewById(R.id.btCadastrar)
+
+        this.btJogar.setOnClickListener({chamarJogo()})
+        this.btCadstrar.setOnClickListener({chamarCadastro()})
+    }
+
+    fun chamarJogo(){
+        val intent = Intent(this, JogoActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun chamarCadastro(){
+        val intent = Intent(this, CadastroActivity::class.java)
+        startActivity(intent)
     }
 }
+
